13afdbfe92ad2072bb6123ce1df388a53f8e3448
thadeus <thadeusnyariki94@gmail.com>
2023-04-20 00:59:28 +0300

    styling basics

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {View, Text, SafeAreaView, StyleSheet} from "react-native";
 
-export default function App() {
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={styles.wrapper}>
+      <View style={styles.container}>
+        <Text>Current Weather</Text>
+      </View>
+    </SafeAreaView>
+  )
 }
 
-const styles = StyleSheet.create({
+const  styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'pink',
+    flex: 1
   },
-});
+
+  wrapper: {
+    flex: 1
+  }
+})
+export default App
