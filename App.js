ab74c6c961d71b77550739a0c7f8beb5a35e8bc6
thadeus <thadeusnyariki94@gmail.com>
2023-04-20 01:32:35 +0300

    The current weather screen

--- App.js
+++ App.js
@@ -6,19 +6,58 @@ const App = () => {
     <SafeAreaView style={styles.wrapper}>
       <View style={styles.container}>
         <Text>Current Weather</Text>
+        <Text style={styles.temp}>6</Text>
+        <Text style={styles.feels}>Feels like 5</Text>
+        <View style={styles.highLowWrapper}>
+          <Text style={styles.highLow}>High: 8</Text>
+          <Text style={styles.highLow}>Low: 6</Text>
+        </View>
+        <View style={styles.bodyWrapper}>
+          <Text style={styles.description}>Its sunny</Text>
+          <Text style={styles.message}>Its perfect t-shirt weather</Text>
+        </View>
       </View>
     </SafeAreaView>
   )
 }
 
 const  styles = StyleSheet.create({
-  container: {
-    backgroundColor: 'pink',
-    flex: 1
-  },
-
   wrapper: {
-    flex: 1
+    flex: 1,
+    backgroundColor: 'pink',
+  },
+  
+  container: {    
+    flex: 1,
+    alignItems: 'center',
+    marginTop: 30
+  }, 
+  temp: {
+    color: 'black',
+    fontSize: 48
+  },
+  feels: {
+    fontSize: 30,
+    color: 'black'
+  },
+  highLow: {
+    color: 'black',
+    fontSize: 20
+  },
+  highLowWrapper: {
+    flexDirection: 'row',
+    gap: 5
+  },
+  bodyWrapper: {
+    justifyContent: 'flex-end',
+    alignItems: 'flex-start'
+  },
+  description: {
+    fontSize: 48
+  },
+  message: {
+    fontSize: 30
   }
 })
+
 export default App
