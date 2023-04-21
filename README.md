5ab28e71a153a920e3e41104f41bfb154765d298
thadeus <thadeusnyariki94@gmail.com>
2023-04-22 01:24:52 +0300

    lists

--- README.md
+++ README.md
@@ -5,3 +5,24 @@
 1. Setting up an app with expo.
 2. Create and import a component.
 3. Basic styling.
+
+## Lists in react-native
+1. FlatList
+2. SectionList
+3. VirtualizedList
+
+
+| FlatList | SectionList |
+| :-----: | :---------: |
+| think  of this as a standard list | when you need a list broken up into sections |
+| required props: Data and renderItem | required props: Sections and renderItem |
+| data must be an array | data must be an array of objects |
+| renderItem displays the list element to the user | RenderItem displays the list element to the user |
+
+## keyExtractor
+- Used to extract a unique key for a given item at the specified index. Key is used for caching and as the react key to track item re-ordering. The default extractor checks item.key, then item.id, and then falls back to using the index, like React does.
+
+### Requirements of keyExtractor
+- must be a string
+- each element must have its own unique ID
+- the key can be added to the data or we can use the keyExtractor prop
--- src/Components/UpcomingWeather.js
+++ src/Components/UpcomingWeather.js
@@ -1,17 +1,88 @@
 import React from 'react'
-import { SafeAreaView, StyleSheet, Text } from "react-native"
+import { SafeAreaView, StyleSheet, Text, FlatList, View, StatusBar } from "react-native"
+import { Feather } from '@expo/vector-icons'
 
-const UpcomingWeather = () => {
+const DATA = [
+    {
+        "dt": 1661871600,
+        "main": {
+          "temp_min": 296.76,
+          "temp_max": 297.87
+        },
+        "weather": [
+          {
+            "main": "Rain"
+          }
+        ],
+        "dt_txt": "2022-08-30 15:00:00"
+      },
+      {
+        "dt": 1661893200,
+        "main": {
+          "temp_min": 290.31,
+          "temp_max": 292.46
+        },
+        "weather": [
+          {
+            "main": "Rain"
+          }
+        ],
+        "dt_txt": "2022-08-30 18:00:00"
+      },
+]
+
+const Item = (props) => {
+    const {dt_txt, min, max} = props
     return (
-        <SafeAreaView styles={styles.container}>
+        <View style={styles.item}>
+            <Feather name={'sun'} size={50} color={'white'}/>
+            <Text style={styles.date}>{dt_txt}</Text>
+            <Text style={styles.temp}>{min}</Text>
+            <Text style={styles.temp}>{max}</Text>
+            <Text></Text>
+        </View>
+    )
+}
+const UpcomingWeather = () => {
+    const renderItem = ({item}) => (
+        <Item condition={item.weather[0].main} dt_txt={item.dt_txt} min={item.main.temp_min} max={item.main.temp_max}/>
+    )
+    return (
+        <SafeAreaView style={styles.container}>
             <Text>Upcoming Weather</Text>
+            <FlatList
+                data={DATA}
+                renderItem={renderItem}
+                keyExtractor={(item) => item.dt_txt}
+            />
         </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        // marginTop: 30,
+        marginTop: StatusBar.currentHeight || 0,
+        backgroundColor: 'red'
+    },
+    item: {
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        borderWidth: 5,
+        backgroundColor: 'pink',
+    },
+    temp: {
+        color: 'white',
+        fontSize: 20
+    },
+    date: {
+        color: 'white',
+        fontSize: 15
     }
 })
 
